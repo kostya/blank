28afc65a1a9d3e6fe060fa974c1234ffb74ef395
Konstantin Makarchev <kostya27@gmail.com>
2018-04-03 18:17:39 +0300

    fix specs

--- spec/blank_spec.cr
+++ spec/blank_spec.cr
@@ -2,41 +2,40 @@ require "./spec_helper"
 
 describe "CoreExt" do
   describe "blank?" do
-    assert { "#{0xa0.chr}".blank?.should be_true }
-    assert { "\t\n".blank?.should be_true }
-    assert { " ".blank?.should be_true }
-    assert { "a".blank?.should be_false }
-    assert { "a ".blank?.should be_false }
-    assert { "".blank?.should be_true }
-    assert { "\t\n #{0xa0.chr} ".blank?.should be_true }
-    assert { "\t\n #{0xa0.chr}1".blank?.should be_false }
+    it { "#{0xa0.chr}".blank?.should be_true }
+    it { "\t\n".blank?.should be_true }
+    it { " ".blank?.should be_true }
+    it { "a".blank?.should be_false }
+    it { "a ".blank?.should be_false }
+    it { "".blank?.should be_true }
+    it { "\t\n #{0xa0.chr} ".blank?.should be_true }
+    it { "\t\n #{0xa0.chr}1".blank?.should be_false }
 
-    assert { nil.blank?.should be_true }
-    assert { 1.blank?.should be_false }
-    assert { [1].blank?.should be_false }
-    assert { ([] of Int32).blank?.should be_true }
+    it { nil.blank?.should be_true }
+    it { 1.blank?.should be_false }
+    it { [1].blank?.should be_false }
+    it { ([] of Int32).blank?.should be_true }
 
-    assert { {1 => 2}.blank?.should be_false }
-    assert { ({} of Int32 => String).blank?.should be_true }
+    it { {1 => 2}.blank?.should be_false }
+    it { ({} of Int32 => String).blank?.should be_true }
   end
 
   describe "present?" do
-    assert { "#{0xa0.chr}".present?.should be_false }
-    assert { "\t\n".present?.should be_false }
-    assert { " ".present?.should be_false }
-    assert { "a".present?.should be_true }
-    assert { "a ".present?.should be_true }
-    assert { "".present?.should be_false }
-    assert { "\t\n #{0xa0.chr} ".present?.should be_false }
-    assert { "\t\n #{0xa0.chr}1".present?.should be_true }
+    it { "#{0xa0.chr}".present?.should be_false }
+    it { "\t\n".present?.should be_false }
+    it { " ".present?.should be_false }
+    it { "a".present?.should be_true }
+    it { "a ".present?.should be_true }
+    it { "".present?.should be_false }
+    it { "\t\n #{0xa0.chr} ".present?.should be_false }
+    it { "\t\n #{0xa0.chr}1".present?.should be_true }
 
-    assert { nil.present?.should be_false }
-    assert { 1.present?.should be_true }
-    assert { [1].present?.should be_true }
-    assert { ([] of Int32).present?.should be_false }
+    it { nil.present?.should be_false }
+    it { 1.present?.should be_true }
+    it { [1].present?.should be_true }
+    it { ([] of Int32).present?.should be_false }
 
-    assert { {1 => 2}.present?.should be_true }
-    assert { ({} of Int32 => String).present?.should be_false }
+    it { {1 => 2}.present?.should be_true }
+    it { ({} of Int32 => String).present?.should be_false }
   end
-
 end
